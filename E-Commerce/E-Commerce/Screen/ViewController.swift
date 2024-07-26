aab2309d014a077f6ffc9b21f8918874565d1afb
Oğuzhan Akın <55364051+Ogushanakin@users.noreply.github.com>
2024-07-26 22:40:40 +0300

    initial commit

---
+++ E-Commerce/E-Commerce/Screen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  E-Commerce
+//
+//  Created by Oğuzhan Akın on 26.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
